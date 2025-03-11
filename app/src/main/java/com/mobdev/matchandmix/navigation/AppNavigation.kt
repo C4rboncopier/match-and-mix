f73421fd7a9777054fb76e3cff41dd5b40c27bc3
C4rboncopier <gLozarita11@gmail.com>
2025-03-11 22:46:04 +0800

    added credits screen and fixed leaderboards file management

--- app/src/main/java/com/mobdev/matchandmix/navigation/AppNavigation.kt
+++ app/src/main/java/com/mobdev/matchandmix/navigation/AppNavigation.kt
@@ -1,5 +1,7 @@
 package com.mobdev.matchandmix.navigation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -27,6 +29,7 @@ sealed class Screen(val route: String) {
     object Instructions : Screen("instructions")
 }
 
+@RequiresApi(Build.VERSION_CODES.Q)
 @Composable
 fun AppNavigation(navController: NavHostController) {
     NavHost(
